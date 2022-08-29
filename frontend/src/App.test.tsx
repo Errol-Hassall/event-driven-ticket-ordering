2e15ca9918e53cbff4628ea756bda14a8eb21279
Errol Hassall <Errol.Hassall99@gmail.com>
2022-08-30 06:32:04 +1000

    Release with github

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders Buy tickets now link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Buy tickets now/i);
   expect(linkElement).toBeInTheDocument();
 });
